6a0fbe0b2540f5a57dacd7f291fbbd13f29e0044
AB090137 <artem_belmeha@epam.com>
2022-10-03 21:34:00 +0300

    add more

---
+++ src/my/r/Lesson2/Rrrrr.java
@@ -0,0 +1,5 @@
+package my.r.Lesson2;
+
+public class Rrrrr {
+    //task from lesson2
+}
--- src/my/r/Main.java
+++ src/my/r/Main.java
@@ -5,7 +5,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        myMethod(1,2, 3,5,6,7,8,10);
+        myMethod(1,2);
+        myMethod(1,2,4);
+    }
 
+//    public static int myMethod(int a, int b, int c) {
+    public static int myMethod(int a, int b, int... c) {
+//    public static int myMethod(int[] c) {
+        return a + b;
     }
 
 }
